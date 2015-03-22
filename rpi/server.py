5debd8c690ee9e39b40a1fe4e19fab23b81dad91
Gavin Cabbage <gbot@gbot.(none)>
2015-03-22 14:21:53 +0000

    streaming stuff

--- rpi/server.py
+++ rpi/server.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+
+from camera import Camera
+
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
-def root():
-	return render_template('root.html')
+def index():
+	return render_template('index.html')
+
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
