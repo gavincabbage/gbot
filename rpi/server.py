880ec903997d5c65bc85d19dc846de1d4b2db1f6
gbot <gbot@gbot.(none)>
2015-03-22 14:16:18 +0000

    streaming start

--- rpi/server.py
+++ rpi/server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def root():
-	render_template('root.html')
+	return render_template('root.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
