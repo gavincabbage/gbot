fad959c8ecfbd7aefbbf98538f352162eb38e3d3
Gavin Cabbage <gavincabbage@users.noreply.github.com>
2015-04-05 01:15:23 -0400

    Update __init__.py

--- feed/__init__.py
+++ feed/__init__.py
@@ -2,14 +2,21 @@ __all__ = ['camera']
 
 
 from os import getenv
-from flask import Flask
+from flask import Flask, render_template, Response
 
 
 app = Flask(__name__)
-
 
 config_file = getenv('GBOT_ROOT_DIR') + '/config/base.py'
 app.config.from_pyfile(config_file)
 
 
-from feed.routes import *
+@app.route('/feed')
+def feed():
+
+    if app.config['CAMERA_ENABLED']:
+        from feed.camera import generate_feed, Camera
+        return Response(generate_feed(Camera()),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+    else:
+        return str(), 200
