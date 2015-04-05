72272636389c82a202a9cb5c3ec50a3bd21350f3
Gavin Cabbage <gavincabbage@gmail.com>
2015-04-04 21:28:04 -0400

    runSpeed sketch stuff

--- sketch/gbot.h
+++ sketch/gbot.h
@@ -9,28 +9,21 @@
 #define SERVO_PIN         10
 
 #define SERIAL_SPEED      9600
-//#define RIGHT_TARGET      20000
-//#define LEFT_TARGET       -20000
-//#define SPEED             500.0
-#define MAX_SPEED         1000.0
-//#define ACCELERATION      10000.0
 #define SERVO_CENTER      90
 #define SERVO_LEFT        120
 #define SERVO_RIGHT       60
 #define SERVO_INTERVAL    5000
 
+#define MAX_SPEED         1000.0
 #define SPD_LEFT_FORWARD  -750.0
 #define SPD_LEFT_BACK     750.0
 #define SPD_LEFT_LEFT	  400.0
 #define SPD_LEFT_RIGHT    -400.0
-
 #define SPD_RIGHT_FORWARD 750.0
 #define SPD_RIGHT_BACK    -750.0
-#define SPD_RIGHT_LEFT	  -400.0
-#define SPD_RIGHT_RIGHT   400.0
-
+#define SPD_RIGHT_LEFT	  400.0
+#define SPD_RIGHT_RIGHT   -400.0
 #define SPD_STOP		  0.0
-
 
 #define MOVE_FORWARD      10
 #define MOVE_BACK         11
