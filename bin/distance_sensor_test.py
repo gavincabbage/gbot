c9ca523164d0d3f0fb4970e1515c6266eb138721
Gavin Cabbage <gavin.cabbage@capitalone.com>
2015-04-11 13:57:36 -0400

    pulse fix

--- bin/distance_sensor_test.py
+++ bin/distance_sensor_test.py
@@ -17,14 +17,19 @@ time.sleep(2)
 GPIO.output(TRIG, True)
 time.sleep(0.00001)
 GPIO.output(TRIG, False)
- 
+
+pulse_start = time.time()
 while GPIO.input(ECHO)==0:
   pulse_start = time.time()
- 
+
+pulse_end = time.time()
 while GPIO.input(ECHO)==1:
   pulse_end = time.time()
  
 pulse_duration = pulse_end - pulse_start
+print "Start: ", pulse_start
+print "End: ", pulse_end
+print "Duration: ", pulse_duration
  
 distance = pulse_duration * 17150
  
