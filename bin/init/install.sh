b7153d38e40b48c09ac4965b68a776f6bce1e429
Gavin Cabbage <gavin.cabbage@capitalone.com>
2015-04-25 22:32:43 -0400

    config setup for init work:

--- bin/init/install.sh
+++ bin/init/install.sh
@@ -6,7 +6,7 @@ GBOT_ROOT_DIR=/home/gbot/gbot
 GBOT_INIT=$GBOT_ROOT_DIR/bin/init
 INIT_DIR=/etc/init.d
 
-mv $GBOT_INIT/gbot.sh $INIT_DIR/gbot
+cp $GBOT_INIT/gbot.sh $INIT_DIR/gbot
 
 chmod 777 $INIT_DIR/gbot
 
