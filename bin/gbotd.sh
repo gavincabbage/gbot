d49a57014ddf81874feb6ca7da5f18ebe4f499ec
Gavin Cabbage <gavin.cabbage@capitalone.com>
2015-04-25 23:55:54 -0400

    init script fixes

--- bin/gbotd.sh
+++ bin/gbotd.sh
@@ -5,5 +5,4 @@ export GBOT_ROOT_DIR=/home/gbot/gbot
 gbot_logfile="/tmp/gbot_log.txt"
 
 cd ${GBOT_ROOT_DIR}
-venv/bin/gunicorn --timeout 3600 -w 2 -b 0.0.0.0:8088 \
-        --error-logfile ${gbot_logfile} --access-logfile ${gbot_logfile} web:app > ${gbot_logfile} 2>&1
+venv/bin/gunicorn --timeout 3600 -w 2 -b 0.0.0.0:8088 --error-logfile - --access-logfile - web:app >> ${gbot_logfile} 2>&1
