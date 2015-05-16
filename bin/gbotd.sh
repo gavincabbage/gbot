716abe6a61a4a68f32de49f1f9d91c3620a00492
Gavin Cabbage <gavin.cabbage@capitalone.com>
2015-05-16 18:07:33 -0400

    redis config for feed

--- bin/gbotd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export GBOT_ROOT_DIR=/home/gbot/gbot
-
-gbot_logfile="/tmp/gbot_log.txt"
-
-cd ${GBOT_ROOT_DIR}
-venv/bin/gunicorn --timeout 3600 -w 2 -b 0.0.0.0:8088 --error-logfile - --access-logfile - web:app >> ${gbot_logfile} 2>&1
