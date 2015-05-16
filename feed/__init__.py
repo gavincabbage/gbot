cc54fe3cb9e74156035fb4558615f223d82eee24
Gavin Cabbage <gavincabbage@gmail.com>
2015-05-16 20:19:39 +0000

    redis py test

--- feed/__init__.py
+++ feed/__init__.py
@@ -3,6 +3,7 @@ __all__ = ['app']
 
 from os import getenv
 from flask import Flask
+from redis import StrictRedis
 
 
 app = Flask(__name__)
@@ -10,5 +11,7 @@ app = Flask(__name__)
 config_file = getenv('GBOT_ROOT_DIR') + '/config/base.py'
 app.config.from_pyfile(config_file)
 
+redis = StrictRedis(host='localhost', port=8090, db=0)
+redis.set('feed_started', 'true')
 
 import feed.routes
