e8de1c194b5cb729a5763c2586bcdef01fb95e93
Gavin Cabbage <gavin.cabbage@capitalone.com>
2015-05-17 19:45:20 -0400

    feed redis config fix

--- bin/startup.sh
+++ bin/startup.sh
@@ -12,26 +12,38 @@ log()
     echo ${name}": $1" >> ${logfile} 2>&1
 }
 
-start()
+load_config_into_redis()
 {
-    cd ${GBOT_ROOT_DIR}
-
     log "loading config into reddis..."
     venv/bin/python bin/load_redis.py config/config.json >> ${logfile} 2>&1
+}
 
+start_feed()
+{
     log "starting feed..."
     venv/bin/gunicorn --timeout 3600 -w 2 -b 0.0.0.0:8089 \
             --error-logfile ${logfile} --access-logfile ${logfile} feed:app &
     feed_pid="$!"
     log "feed_pid=${feed_pid}"
     redis-cli -p 8090 -n 0 set feed_pid "${feed_pid}" >> ${logfile} 2>&1
+}
 
+start_web()
+{
     log "starting web..."
     venv/bin/gunicorn --timeout 3600 -w 2 -b 0.0.0.0:8088 \
             --error-logfile ${logfile} --access-logfile ${logfile} web:app &
     web_pid="$!"
     log "web_pid=${web_pid}"
     redis-cli -p 8090 -n 0 set web_pid "${web_pid}" >> ${logfile} 2>&1
+}
+
+start()
+{
+    cd ${GBOT_ROOT_DIR}
+    load_config_into_redis
+    start_feed
+    start_web
 }
 
 
