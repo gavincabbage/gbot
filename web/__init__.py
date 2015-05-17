96db516ebe12cca1b44422d89c34021c53596130
Gavin Cabbage <gavin.cabbage@capitalone.com>
2015-05-16 20:50:23 -0400

    converted web fixes

--- web/__init__.py
+++ web/__init__.py
@@ -13,21 +13,21 @@ app = Flask(__name__)
 
 redis = StrictRedis(host='localhost', port=8090, db=0)
 app.config['DEBUG'] = get_bool('web.debug', redis)
-app.config['I2C_ENABLED'] = get_bool('core.i2c_enabled', redis)
-app.config['I2C_ENABLED'] = get_bool('core.distance_sensor_enabled', redis)
 app.config['FEED_URL'] = redis.get('feed.paths.root')
+i2c_enabled = get_bool('core.i2c_enabled', redis)
+distance_sensor_enabled = get_bool('core.distance_sensor_enabled', redis)
 trig_pin = int(redis.get('core.trig_pin'))
 echo_pin = int(redis.get('core.echo_pin'))
 
 
 i2c_controller = None
-if app.config['I2C_ENABLED']:
+if i2c_enabled:
     from externals.i2c_controller import I2CController
     i2c_controller = I2CController()
 
 
 distance_sensor = None
-if app.config['DISTANCE_SENSOR_ENABLED']:
+if distance_sensor_enabled:
     from externals.distance_sensor import DistanceSensor
     distance_sensor = DistanceSensor(trig_pin, echo_pin)
 
