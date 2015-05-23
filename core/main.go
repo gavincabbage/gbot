864ec5cb54b306b8e03b51a3c9ce535224bfd81f
Gavin Cabbage <gavincabbage@gmail.com>
2015-05-23 17:10:07 +0000

    golang i2c test

--- core/main.go
+++ core/main.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/kidoman/embd"
+	_ "github.com/kidoman/embd/host/rpi"
 )
 
+var bus embd.I2CBus
+
 func lookHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("ENTER pageHandler")
+	fmt.Println("ENTER lookHandler")
+	bus.WriteByte(0x04, 0x0d)
 
 }
 
 func moveHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("ENTER stubHandler")
+	fmt.Println("ENTER moveHandler")
 
 }
 
 func main() {
 	
 	// TODO:: load config from redis 
+	
+	bus = embd.NewI2CBus(1)
+	bus.WriteByte(0x04, 0x0a)
 	
 	r := mux.NewRouter()
 	r.HandleFunc("/look/{direction}", lookHandler).Methods("GET")
